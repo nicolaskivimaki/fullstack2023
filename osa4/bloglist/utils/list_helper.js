c4e9c71e851f056b14304be405258e3031d08504
nicolaskivimaki <kivimakinicolas@gmail.com>
2024-01-06 22:40:37 +0200

    4.3-4.7

--- osa4/bloglist/utils/list_helper.js
+++ osa4/bloglist/utils/list_helper.js
@@ -2,7 +2,75 @@
 const dummy = (blogs) => {
     return 1
   }
+
+const totalLikes = (blogs) => {
+return blogs.reduce((sum, blog) => sum + blog.likes, 0);
+  }
+
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+      return null;
+    }
   
-  module.exports = {
-    dummy
+    let favorite = blogs[0];
+  
+    blogs.forEach(blog => {
+      if (blog.likes > favorite.likes) {
+        favorite = blog;
+      }
+    });
+  
+    return {
+      title: favorite.title,
+      author: favorite.author,
+      likes: favorite.likes
+    };
+  }
+
+const ld = require('lodash');
+
+const mostBlogs = (blogs) => {
+    if (blogs.length === 0) {
+        return null;
+    }
+
+    const blogCounts = ld.countBy(blogs, 'author');
+    const authorWithMostBlogs = ld.maxBy(Object.keys(blogCounts), (author) => blogCounts[author]);
+
+    return {
+        author: authorWithMostBlogs,
+        blogs: blogCounts[authorWithMostBlogs]
+    };
+    }
+
+const mostLikes = (blogs) => {
+    const likesByAuthor = {};
+    
+    blogs.forEach(blog => {
+        if (!likesByAuthor[blog.author]) {
+            likesByAuthor[blog.author] = 0;
+        }
+        likesByAuthor[blog.author] += blog.likes;
+    });
+    
+    let maxLikes = 0;
+    let maxAuthor = '';
+    
+    for (const author in likesByAuthor) {
+        if (likesByAuthor[author] > maxLikes) {
+        maxLikes = likesByAuthor[author];
+        maxAuthor = author;
+        }
+    }
+    
+    return maxLikes === 0 ? null : { author: maxAuthor, likes: maxLikes };
+    }
+      
+
+module.exports = {
+    dummy, 
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
+    mostLikes,
   }
